650444ad88cab34e0778159467f2e1c2f42b5c4a
0xhelloweb3 <1245816264@qq.com>
2024-01-19 11:56:11 +0800

    feat: valid bitwork

--- lib/utils/atomical-format-helpers.ts
+++ lib/utils/atomical-format-helpers.ts
@@ -342,25 +342,25 @@ export const hasValidBitwork = (txid, bitwork: string, bitworkx: number) => {
       return true;
     } else {
       const next_char = txid[bitwork.length]
-      const char_map = {
-        '0': 0,
-        '1': 1,
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        'a': 10,
-        'b': 11,
-        'c': 12,
-        'd': 13,
-        'e': 14,
-        'f': 15
-      }
-      const get_numeric_value = char_map[next_char]
+      // const char_map = {
+      //   '0': 0,
+      //   '1': 1,
+      //   '2': 2,
+      //   '3': 3,
+      //   '4': 4,
+      //   '5': 5,
+      //   '6': 6,
+      //   '7': 7,
+      //   '8': 8,
+      //   '9': 9,
+      //   'a': 10,
+      //   'b': 11,
+      //   'c': 12,
+      //   'd': 13,
+      //   'e': 14,
+      //   'f': 15
+      // }
+      const get_numeric_value = parseInt(next_char, 16);
       if (get_numeric_value >= bitworkx) {
         return true;
       }
